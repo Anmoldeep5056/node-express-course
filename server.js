7b295cf7f83fb8aa691650d3b059e52ef5892e28
Anmol <anmolmahant97@gmail.com>
2021-07-30 09:30:46 +0530

    add first GET route

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");// gives me access to the express library by searching my node_module for express
-const app = express();// creates an instance for express constructor
+const app = express();// creates an instance for express server
 
 
+
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
 
 
 app.listen(8000, function(){
-	console.log("server is listening")
-})//this starts the server locally on the port 8000, base url becomes localhost:8000
-
-
+	console.log("server is listening")})
